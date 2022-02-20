bb6e6a95fa3c9eed6f7198361023cbeb02468e64
Phil <happy.melodist@gmail.com>
2022-02-20 10:41:20 -0600

    add correct images and progress through creation of the Layer class

---
+++ script.js
@@ -0,0 +1,63 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "layer-5.png";
+
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+  update() {
+    this.speed = gameSpeed * this.speedModifier;
+    if (this.x <= -this.width) {
+      this.x = this.width + this.x2 - this.speed;
+    }
+    if (this.x2 <= -this.width) {
+      this.x2 = this.width + this.x - this.speed;
+    }
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
+  }
+  draw() {}
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+// =-=-=-=-=-=-=-=-=-= boneyard of refactored code -=--=-=-=-=-=-=-=-=-
+
+/*
+let x = 0;
+let x2 = 2400;
+
+  ctx.drawImage(backgroundLayer4, x, 0);
+  ctx.drawImage(backgroundLayer4, x2, 0);
+  
+  if (x < -2400) x = 2400 + x2 - gameSpeed;
+  else x -= gameSpeed;
+  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+  else x2 -= gameSpeed;
+*/
