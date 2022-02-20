76cb37f257ed42d0777a8532b5b66d498f7f49f8
Phil <happy.melodist@gmail.com>
2022-02-20 10:57:22 -0600

    create the first version of this project's parallax effect by iterating over the five instantiated new Layers

--- script.js
+++ script.js
@@ -37,11 +37,26 @@ class Layer {
     this.x = Math.floor(this.x - this.speed);
     this.x2 = Math.floor(this.x2 - this.speed);
   }
-  draw() {}
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+  }
 }
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  gameObjects.forEach((boobie) => {
+    boobie.update();
+    boobie.draw();
+  });
   requestAnimationFrame(animate);
 }
 
